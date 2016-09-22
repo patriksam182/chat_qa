fc0c32713e2add3760c892db21a8b76a49bde8a0
Patrik Samuel <chextersa@gmail.com>
2016-09-22 16:41:57 -0600

    commit 00888

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/', routes.index);
 
 //Asignamos a server la creación del servidor http.
 var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("puerto que esta escuchando ahorrrrr  okiiiii samsasmasmasma otrotort" + app.get('port'));
+  console.log("puerto que esta escuchando ahorrrrr  okiiiii samsasmasmasma otrotort   fff" + app.get('port'));
 });
 
 //Importamos socket.io utilizando el servidor creado anteriormente.
